07ceead2915e9808bd186c190a64e6fb596c2a15
Brittany Servent <bservent@gmail.com>
2021-01-26 10:39:23 -0800

    stack & queue

---
+++ stack_queues.js
@@ -0,0 +1,3 @@
+//QUEUE = like queue in line = first in first out = Therefore data flows from bottom to top. Elements are added to rear and removed from front
+
+//STACK = opposite from queue = push/pop (insertion and deletion)from same side = first in last out or list in first out
