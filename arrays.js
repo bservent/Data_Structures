1f1f64480975f47ff3e2e326a81aae96e48326ae
Brittany Servent <bservent@gmail.com>
2021-01-26 11:01:54 -0800

    arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,26 @@
 //Arrays
 
-//data structure that is linear and 
+//set of similar values grouped together (each value = element)
+//data structure that is linear and static
+//once instantiated, the size of the array cannot change(limiting factor)
+//populate them first or populate later
+
+//O NOTATION
+
+//Accessing = 0(1) = index used to complete method in one operation
+
+//Searching = 0(n) = linear search with array not sorted = could take as many operations as the length of array
+
+//Inserting = 0(n) = same reasoning as search
+
+//Deleting = 0(n) = same as search (must tranverse over array to find item to delet)
+
+//Pros for Arrays
+//good to store similar contiguous data
+//0(1) accessing power is great
+//basic & easy to learn
+
+//Cons for Arrays
+//size can't be changed once initialized
+//insert/delete are not efficient
+//can waste storage space
